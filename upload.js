a551d8074337403c50a5351f867babc96e3a31cd
Hussein Rashid. <113936853+h0-qp@users.noreply.github.com>
2026-06-20 19:31:23 +0300

    .

--- upload.js
+++ upload.js
@@ -3,9 +3,14 @@ try {
     console.log('Fetching server...');
     const serversRes = execSync('curl -s https://api.gofile.io/servers');
     const servers = JSON.parse(serversRes.toString());
-    const serverName = servers.data.servers[0].name;
+    
+    // Pick a random server to avoid hanging servers
+    const serverList = servers.data.servers;
+    const serverName = serverList[Math.floor(Math.random() * serverList.length)].name;
+    
     console.log('Got server:', serverName, 'Uploading...');
-    const output = execSync(`curl -s -F "file=@./app/build/outputs/apk/debug/app-debug.apk" https://${serverName}.gofile.io/contents/uploadfile`);
+    // Add -m 45 for max time 45 seconds to avoid silent timeouts
+    const output = execSync(`curl -m 45 -s -F "file=@./app/build/outputs/apk/debug/app-debug.apk" https://${serverName}.gofile.io/contents/uploadfile`);
     console.log("URL:", output.toString());
 } catch (e) {
     console.error(e);
